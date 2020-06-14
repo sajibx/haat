a4edfd9c3f557e59cabff9dae335decc853d3bf8
sajibx <ashiqur.sajib@gmail.com>
2020-06-14 16:28:18 +0600

    sajib

--- src/main/kotlin/com/ss/haat/HaatApplication.kt
+++ src/main/kotlin/com/ss/haat/HaatApplication.kt
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.web.filter.CommonsRequestLoggingFilter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -42,3 +44,17 @@ class WebConfig : WebMvcConfigurer {
 //		registry.addMapping("/**")
 //	}
 //}
+
+@Bean
+fun requestLoggingFilter(): CommonsRequestLoggingFilter {
+	val loggingFilter = CommonsRequestLoggingFilter()
+	loggingFilter.setIncludeClientInfo(true)
+	loggingFilter.setIncludeQueryString(true)
+	loggingFilter.setIncludePayload(true)
+	return loggingFilter
+}
+
+@EnableJpaRepositories
+class Config {
+
+}
