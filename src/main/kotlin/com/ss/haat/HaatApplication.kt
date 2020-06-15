d03930499292662ddaf1c49d9ea7a3799bd0fdfd
sajibx <ashiqur.sajib@gmail.com>
2020-06-15 11:55:19 +0600

    sajib

--- src/main/kotlin/com/ss/haat/HaatApplication.kt
+++ src/main/kotlin/com/ss/haat/HaatApplication.kt
@@ -19,7 +19,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Controller
 @SpringBootApplication
-class HaatApplication: SpringBootServletInitializer(), WebApplicationInitializer {
+class HaatApplication: SpringBootServletInitializer() {
 
 	@Autowired
 	lateinit var fileStorage: FileStorage
