b58b0ee7ee10fb680dc0ea93ca4fa76dce2c02d7
sajibx <ashiqur.sajib@gmail.com>
2020-06-15 10:16:37 +0600

    sajib

--- src/main/kotlin/com/ss/haat/HaatApplication.kt
+++ src/main/kotlin/com/ss/haat/HaatApplication.kt
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.filter.CommonsRequestLoggingFilter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
@@ -22,6 +24,13 @@ class HaatApplication: SpringBootServletInitializer() {
 	lateinit var fileStorage: FileStorage
 	fun main(args: Array<String>) {
 		runApplication<HaatApplication>(*args)
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	fun res() :String
+	{
+		return "hey"
 	}
 //	@Bean
 //	fun run() = CommandLineRunner {
