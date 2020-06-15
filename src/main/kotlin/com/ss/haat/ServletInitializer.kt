24f33c13414610205d604accbc08be88001ce259
sajibx <ashiqur.sajib@gmail.com>
2020-06-15 10:58:57 +0600

    sajib

--- src/main/kotlin/com/ss/haat/ServletInitializer.kt
+++ src/main/kotlin/com/ss/haat/ServletInitializer.kt
@@ -1,12 +1,23 @@
 package com.ss.haat
 
+import org.springframework.boot.Banner
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
 
 class ServletInitializer : SpringBootServletInitializer() {
 
 	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
 		return application.sources(HaatApplication::class.java)
+	}
+
+
+	fun main(args: Array<String>) {
+		configureApplication(SpringApplicationBuilder()).run(*args)
+	}
+
+	private fun configureApplication(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+		return builder.sources(HaatApplication::class.java).bannerMode(Banner.Mode.OFF)
 	}
 
 }
